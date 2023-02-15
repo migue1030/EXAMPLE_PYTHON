befe47e3c78f26e4f379642cf904e3f986e43747
migue1030 <miguelangelmolina1030@gmail.com>
2023-02-15 09:18:18 -0500

    primer commit

---
+++ ejercicio_suma.py
@@ -0,0 +1,11 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+nl=0
+n2=0
+suma=0
+print("ingresa el primer numero")
+n1=int(input())
+print("ingresa el segundo numero")
+n2=int(input())
+suma=n1+n2
+print("la suma=",suma)
